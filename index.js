8f53fb53023878d288951d642236e09d11b36689
bokuweb <bokuweb12@gmail.com>
2017-06-08 20:54:41 +0900

    Fix hash

--- index.js
+++ index.js
@@ -3,36 +3,28 @@
 const { execSync } = require('child_process');
 
 const current = execSync('git branch | grep \"^\\*\" | cut -b 3-', { encoding: 'utf8' });
-const shownBranches = execSync('git show-branch -a --sha1-name', { encoding: 'utf8' }).split(/\n/);
-const separatorIndex = shownBranches.findIndex((b) => /^--/.test(b));
-const branches = shownBranches.slice(0, separatorIndex)
-  .map((b, i) => {
-    const name = b.replace(/\].+/, '').match(/\[(.+)/)[1];
-    return {
-      order: i,
-      name,
-      isCurrent: b[i] === '*',
-      hash: execSync(`git rev-parse ${name}`, { encoding: 'utf8' }).replace('\n', ''),
+let baseHash;
+
+execSync('git show-branch -a --sha1-name | grep "*" | grep "+"', { encoding: 'utf8' })
+  .split(/\n/)
+  .map(commit => {
+    if (!commit) return;
+    console.log(commit)
+    return commit.replace(/\].+/, '').match(/\[(.+)/)[1];
+  })
+  .some(hash => {
+    execSync(`git checkout ${hash}`);
+    let result;
+    try {
+      result = execSync(`git merge-base --fork-point ${current}`, { encoding: 'utf8' });
+    } catch (e) {
+      // nop
     }
+    console.log(result)
+    baseHash = result;
+    return !!result;
   });
-const currentIndex = branches.find(b => b.isCurrent).order;
-const currentHash = branches[currentIndex].hash;
-const commits = shownBranches.slice(separatorIndex + 1, shownBranches.length - 1);
-const baseShortHash = commits.find(c => {
-  const [status, branch] = c.replace(/\].+/, '').split('[');
-  const isCurrent = status[currentIndex] === '*' || status[currentIndex] === '-';
-  if (!isCurrent) return;
-  const length = [...status].map((s, i) => {
-    if (i === currentIndex) return;
-    if (s === ' ') return;
-    const { name } = branches[i];
-    const hash = execSync(`git rev-parse ${name}`, { encoding: 'utf8' }).replace('\n', '');
-    if (hash === currentHash) return;
-    return true;
-  }).filter(s => !!s).length;
-  return length;
-}).replace(/\].+/, '').match(/\[(.+)/)[1];
 
-const baseHash = execSync(`git rev-parse ${baseShortHash}`, { encoding: 'utf8' }).replace('\n', '');
-
+execSync(`git checkout ${current}`);
+console.log(baseHash)
 process.stdout.write(baseHash);
