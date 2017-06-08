c0b8b470d2cbf2d3df7c62b26bc50d070de50224
bokuweb <bokuweb12@gmail.com>
2017-06-08 22:09:58 +0900

    Fix test

--- spec/fixture0.spec.js
+++ spec/fixture0.spec.js
@@ -1,22 +1,17 @@
 import test from 'ava';
 import { execFile, execSync } from 'child_process';
-var process = require('process');
+import process from 'process';
+
 process.chdir('spec/fixtures/fixture0');
-
-// execSync('cd spec/fixtures/fixture0');
-
-// console.log(execSync('git status', { encoding: 'utf8' }));
-console.log(execSync('ls', { encoding: 'utf8' }));
 
 test.beforeEach(t => {
   execSync('git checkout topic');
 });
 
-test.serial('', async t => {
+test.serial('should get expected hash', async t => {
   const stdout = await new Promise((resolve) => {
     execFile('../../../index.js', (error, stdout) => {
       if (error) console.error(error);
-      console.log(stdout)
       resolve(stdout)
     });
   });
