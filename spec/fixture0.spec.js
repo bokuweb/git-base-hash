e75460dadd85113384944c69d68aa083d2a5df22
bokuweb <bokuweb12@gmail.com>
2017-06-09 10:50:39 +0900

    Add comment

--- spec/fixture0.spec.js
+++ spec/fixture0.spec.js
@@ -8,6 +8,33 @@ test.beforeEach(t => {
   execSync('git checkout topic');
 });
 
+/* Test case 0
+
+- result of show-branch
+
+! [bravo] bravo1
+ ! [master] master1
+  * [topic] topic2
+---
+  * [c8da61d] topic2
+ +* [fe903eb] master1
+  * [c3f7cf7] topic1
++ * [dd12339] bravo1
+++* [5e7fab3] init
+
+- result of log --graph
+
+* c8da61d - (HEAD -> topic) topic2 (18 minutes ago) <bokuweb>
+*   d0c5c98 - merge 'master' into topic (19 minutes ago) <bokuweb>
+|\
+| * fe903eb - (master) master1 (22 minutes ago) <bokuweb>
+* | c3f7cf7 - topic1 (20 minutes ago) <bokuweb>
+* | dd12339 - (bravo) bravo1 (21 minutes ago) <bokuweb>
+|/
+* 5e7fab3 - init (23 minutes ago) <bokuweb>
+
+*/
+
 test.serial('should get expected hash', async t => {
   const stdout = await new Promise((resolve) => {
     execFile('../../../index.js', (error, stdout) => {
