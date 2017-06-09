b9ee7e3631ddc2acac0b99e6c598760a81f9f6e8
bokuweb <bokuweb12@gmail.com>
2017-06-09 18:00:26 +0900

    aaaaaa

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const shownBranches = execSync('git show-branch -a --sha1-name', { encoding: 'ut
 const separatorIndex = shownBranches.findIndex((b) => /^--/.test(b));
 const branches = [];
 const currentHash = execSync(`git rev-parse ${currentName}`, { encoding: 'utf8' }).replace('\n', '');
-const firstParentHashes = execSync('git log --oneline --first-parent', { encoding: 'utf8' }).split('\n').map(log => log.split(' ')[0]);
+const firstParentHashes = execSync('git log -n 1000 --oneline --first-parent', { encoding: 'utf8' }).split('\n').map(log => log.split(' ')[0]);
 
 let currentIndex;
 let baseHash = '';
@@ -20,7 +20,7 @@ shownBranches.slice(0, separatorIndex)
     branches.push(name);
   });
 
-shownBranches
+const candidateHashes = shownBranches
   .slice(separatorIndex + 1, shownBranches.length - 1)
   .filter(b => {
     const [status, branch] = b.replace(/\].+/, '').split('[');
@@ -37,11 +37,33 @@ shownBranches
       })
       .filter(s => !!s).length;
   })
-  .map(b => b.replace(/\].+/, '').match(/\[(.+)/)[1])
-  .some(hash => {
-    if (firstParentHashes.indexOf(hash) === -1) return;
-    baseHash = execSync(`git rev-parse ${hash}`, { encoding: 'utf8' }).replace('\n', '');
-    return true;
-  });
+  .map(b => b.replace(/\].+/, '').match(/\[(.+)/)[1]);
 
-process.stdout.write(baseHash.trim());
+
+candidateHashes.some(hash => {
+  if (firstParentHashes.indexOf(hash) === -1) return;
+  // baseHash = execSync(`git rev-parse ${hash}`, { encoding: 'utf8' }).replace('\n', '');
+  baseHash = hash;
+  return true;
+});
+
+const parents = execSync('git log -n 1000 --graph --pretty=format:"%h %p"', { encoding: 'utf8' })
+  .split('\n')
+  .map(hashes => (
+    hashes
+      .replace(/\*|\/|\||/g, '')
+      .split(' ')
+      .filter(hash => !!hash)
+  ));
+
+const findParentNode = (parentHash) => parents.find(([hash]) => hash === parentHash);
+
+
+
+candidateHashes.forEach((candidateHash) => {
+  const [target, ...parentHashes] = parents.find(([hash]) => hash === candidateHash);
+  console.log(target)
+  console.log(parentHashes)
+});
+
+// process.stdout.write(baseHash.trim());
