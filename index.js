d7dea9e9a732118ae68ff87bdf27d8afb864e401
bokuweb <bokuweb12@gmail.com>
2017-06-12 10:01:03 +0900

    .

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const candidateHashes = shownBranches
   .slice(separatorIndex + 1, shownBranches.length - 1)
   .filter(b => {
     const [status, branch] = b.replace(/\].+/, '').split('[');
-    const isCurrent = status[currentIndex] === '*'; // || status[currentIndex] === '-';
+    const isCurrent = status[currentIndex] === '*' || status[currentIndex] === '-';
     if (!isCurrent) return;
     return [...status]
       .map((s, i) => {
