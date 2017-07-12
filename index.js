d78311c5b789f28183c98e53dc23895296832a46
ukyo <ukyo.web@gmail.com>
2017-07-12 20:41:32 +0900

    refactoring

--- index.js
+++ index.js
@@ -1,81 +1,46 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process');
+const { Repository } = require('tiny-commit-walker');
 
-let _revParseCache = { };
-function revParse(name) {
-  if (!_revParseCache[name]) {
-    _revParseCache[name] = execSync(`git rev-parse ${name}`, { encoding: 'utf8' }).replace('\n', '');
-  }
-  return _revParseCache[name];
+const repo = new Repository(process.cwd());
+const head = repo.readHeadSync();
+if (head.type === 'commit') {
+  throw new Error('HEAD is not a ref to branch.');
 }
+const commits = repo
+  .readBranchesSync()
+  .filter(b => b.name !== head.branch.name)
+  .map(b => b.commit);
 
-const currentName = execSync('git branch | grep \"^\\*\" | cut -b 3-', { encoding: 'utf8' });
-const shownBranches = execSync('git show-branch -a --sha1-name', { encoding: 'utf8' }).split(/\n/);
-const separatorIndex = shownBranches.findIndex((b) => /^--/.test(b));
-const branches = [];
-const currentHash = revParse(currentName);
-const firstParentHashes = execSync('git log -n 1000 --oneline', { encoding: 'utf8' }).split('\n').map(log => log.split(' ')[0]);
+const hashDict = {};
 
-let currentIndex;
-let baseHash = '';
-
-shownBranches
-  .slice(0, separatorIndex)
-  .forEach((b, i) => {
-    const name = b.replace(/\].+/, '').match(/\[(.+)/)[1];
-    if (!name) return;
-    if (b[i] === '*') currentIndex = i;
-    branches.push(name);
-  });
-
-const candidateHashes = shownBranches
-  .slice(separatorIndex + 1, shownBranches.length - 1)
-  .filter(b => {
-    const [status, branch] = b.replace(/\].+/, '').split('[');
-    const isCurrent = status[currentIndex] === '*' || status[currentIndex] === '-';
-    if (!isCurrent) return;
-    return [...status]
-      .map((s, i) => {
-        if (i === currentIndex) return;
-        if (s === ' ') return;
-        const name = branches[i];
-        const hash = revParse(name);
-        if (hash === currentHash) return;
-        return true;
-      })
-      .filter(s => !!s).length;
-  })
-  .map(b => b.replace(/\].+/, '').match(/\[(.+)/)[1])
-  .filter(hash => currentHash.indexOf(hash));
-
-candidateHashes
-  .some(hash => {
-    if (firstParentHashes.indexOf(hash) === -1) return;
-    baseHash = hash;
-    return true;
-  });
-
-const parents = execSync('git log -n 1000 --graph --pretty=format:"%h %p"', { encoding: 'utf8' })
-  .split('\n')
-  .map(hashes => (
-    hashes
-      .replace(/\*|\/|\||/g, '')
-      .split(' ')
-      .filter(hash => !!hash)
-  ))
-  .filter(hashes => hashes.length);
-
-const findParentNode = (parentHash) => parents.find(([hash]) => hash === parentHash);
-
-const traverseLog = (candidateHash) => {
-  const [target, ...parentHashes] = findParentNode(candidateHash);
-  for (const h of parentHashes) {
-    if (target === baseHash) return true;
-    return traverseLog(h);
+function setupHashes(commit) {
+  if (hashDict[commit.hash]) return;
+  hashDict[commit.hash] = true;
+  if (commit.hasParents) {
+    commit.parentHashes.forEach(hash => {
+      try {
+        setupHashes(commit.walkSync(hash));        
+      } catch (err) { }
+    });
   }
 }
 
-const target = candidateHashes.find(traverseLog);
+commits.forEach(setupHashes);
 
-process.stdout.write(execSync(`git rev-parse ${target}`, { encoding: 'utf8' }).replace('\n', ''));
+let baseHash;
+let commit = head.branch.commit;
+
+while(commit.hasParents) {
+  if (hashDict[commit.hash]) {
+    baseHash = commit.hash;
+    break;
+  }
+  if (commit.isMergeCommit) {
+    baseHash = commit.mergedParentHashes[0];
+    break;
+  }
+  commit = commit.walkSync();
+}
+
+process.stdout.write(baseHash);
