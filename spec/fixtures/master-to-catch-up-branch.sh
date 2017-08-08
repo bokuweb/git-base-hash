122514cf37e2f72b5da656305fdb5748549948ab
bokuweb <bokuweb12@gmail.com>
2017-08-08 15:47:19 +0900

    WIP

--- spec/fixtures/master-to-catch-up-branch.sh
+++ spec/fixtures/master-to-catch-up-branch.sh
@@ -1,3 +1,12 @@
+# *   a025d14 merge master to feat-x
+# |\
+# | * bf6dd42 master2 
+# | * 994654e master1
+# * | bf2cb81 x2 <-- expected!!
+# * | 30d6d40 x1
+# |/
+# * cd8f9f4 first commit
+
 cd spec
 echo "11"
 git init
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,10 +1,12 @@
 import test from 'ava';
-import { execFile, execSync } from 'child_process';
+import { execSync } from 'child_process';
 import process from 'process';
 import { detectBaseHash } from '../lib/index';
 import * as path from 'path';
 import * as rimraf from 'rimraf';
 import * as cpx from 'cpx';
+
+process.chdir('spec');
 
 const specs = [
   'master-to-catch-up-branch',
@@ -12,17 +14,16 @@ const specs = [
 
 specs.map(spec => {
   cpx.copySync(path.resolve(__dirname, 'fixtures', spec), path.resolve(__dirname, '.git'));
-  // execSync(`mv ${path.resolve(__dirname, 'fixtures', spec)} ${path.resolve(__dirname, '.git')}`);
 
   test.serial(spec, async t => {
-    const a = detectBaseHash(__dirname);
-    console.log(a);
-    t.pass();
+    const baseHash = detectBaseHash(__dirname);
+    const expected = execSync('git show-ref --tag', { encoding: "utf8" }).split(' ')[0];
+    t.is(expected, baseHash);
   });
 })
 
 test.afterEach(() => {
-  rimraf.sync(path.resolve(__dirname, '.git'));
+  // rimraf.sync(path.resolve(__dirname, '.git'));
 })
 
 
